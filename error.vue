b744acb8e48f205f6e01b5e299adc5b0e521b3a3
Frank de Boer <frank.de.boer@eo.nl>
2023-09-04 10:03:45 +0200

    added error page

---
+++ error.vue
@@ -0,0 +1,29 @@
+<script setup>
+const error = useError();
+const handleError = () => {
+  clearError({
+    redirect: '/'
+  });
+};
+</script>
+
+<template>
+  <NuxtLayout>
+    <div>
+      <div v-if="error.statusCode === '404'">
+        <h1>{{ error.statusCode }}</h1>
+        <p>Oeps de pagina die je zoekt bestaat helaas niet.</p>
+      </div>
+      <div v-else>
+        <h1>{{ error.statusCode }}</h1>
+        <p>Oeps er lijkt iets fout te zijn gegaan.</p>
+      </div>
+      <div>
+        <p>
+          <strong>{{ error.message }}</strong>
+        </p>
+        <div @click="handleError">Naar de homepage</div>
+      </div>
+    </div>
+  </NuxtLayout>
+</template>
