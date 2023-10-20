9d1a758a4435b8f39401049ac1f8253b73f8e98e
Frank de Boer <frank.de.boer@eo.nl>
2023-10-20 16:43:38 +0200

    ssr false

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@
 export default defineNuxtConfig({
   devtools: { enabled: false },
   modules: ['@nuxtjs/tailwindcss', '@nuxtjs/apollo'],
+  ssr: false,
   apollo: {
     clients: {
         prepr: './apollo/prepr.ts',
