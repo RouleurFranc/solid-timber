5574e82702cad9cbcd133073306348a1aa91abeb
Frank de Boer <frank.de.boer@eo.nl>
2023-10-23 14:14:28 +0200

    apollo fixes

--- pages/index.vue
+++ pages/index.vue
@@ -1,41 +1,45 @@
-
-<script setup>
-  import { computed } from "vue";
-  import { reactive } from "vue";
-  import { GetPageBySlug } from "@/queries/getPageBySlug";
-  import PageHeader from "@/components/PageHeader";
-  import ImageAndText from "@/components/ImageAndText";
-
-  const components = [
-    { name: "PageHeader", comp: PageHeader },
-    { name: "ImageAndText", comp: ImageAndText },
-  ];
-
-  const getComponent = (name) => {
-    const component = components.find((component) => component.name === name);
-    return component ? component.comp : null;
-  };
-
-  const { data } = await useAsyncQuery(
-    GetPageBySlug,
-    {
-      slug: "home",
-    }
-  );
-
-  const page = data.value.Page;
-  const stack = computed(() => {
-    return page.stack;     
-  });
-  </script>
-
-<template>
-  <Header/>
-  <component
-    v-for="element in stack"
-    :key="element._id"
-    :is="getComponent(element.__typename)"
-    :data="element"
-  ></component>
-  <Footer/>
-</template>
+  <script setup>
+    import { computed } from "vue";
+    import { reactive } from "vue";
+    import { GetPageBySlug } from "@/queries/get-page-by-slug";
+  
+    // Import the components
+    import PageHeader from "@/components/PageHeader";
+    import ImageAndText from "@/components/ImageAndText";
+  
+    // Set up the components
+    const components = [
+      { name: "PageHeader", comp: PageHeader },
+      { name: "ImageAndText", comp: ImageAndText },
+    ];
+  
+    // Assign the components for the stack loop above
+    const getComponent = (name) => { 
+      const component = components.find((component) => component.name === name);
+      return component ? component.comp : null;
+    };
+  
+    const { data } = await useAsyncQuery(
+      GetPageBySlug,
+      {
+        slug: "home",
+      }
+    );
+  
+    // Assign the page and stack variables with the queried results
+    const page = data.value.Page;
+    const stack = computed(() => {
+      return page.stack;
+    });
+    </script>
+  
+  <template>
+    <Header/>
+    <component
+      v-for="element in stack"
+      :key="element._id"
+      :is="getComponent(element.__typename)"
+      :data="element"
+    ></component>
+    <Footer/>
+  </template>
---
+++ queries/get-page-by-slug.js
@@ -0,0 +1,36 @@
+// ./queries/get-page-by-slug.js
+
+export const GetPageBySlug = gql`
+  query ($slug: String) {
+    Page(slug: $slug) {
+      _id
+      title
+      _slug
+
+      # Retrieve the stack and two components: 
+      # Page Header and Image and Text 
+      stack {
+        __typename
+        ... on PageHeader  {
+          heading
+          cta_url
+          cta_label
+          image {
+            url(width: 1600)
+          }
+          _id
+          text
+        }
+        ... on ImageAndText {
+          image {
+            url(width: 800)
+          }
+          text
+          title
+          image_position
+          _id
+        }
+      }
+    }
+  }
+`;
