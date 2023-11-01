8e4b6585d3740549dcff1a17fdb11ea27069c8c9
Frank de Boer <frank.de.boer@eo.nl>
2023-11-01 14:46:32 +0100

    project detail page part one

--- pages/projects/[slug].vue
+++ pages/projects/[slug].vue
@@ -1,9 +1,34 @@
+<script setup lang="ts">
+import GET_PROJECT from './getProject.graphql'
+
+const route = useRoute()
+const variables = {
+  slug: route.params.slug,
+}
+const { error, data } = await useAsyncQuery(
+  GET_PROJECT,
+  variables,
+  `getProject_${route.params.slug}`,
+)
+const project = data?.value?.Project
+const projectContent = project.content
+
+console.log(projectContent)
+</script>
 <template>
-  <section>
-    <div
-      class="relative mx-auto max-w-screen-xl items-center px-4 py-16 md:py-20 lg:px-6 xl:gap-16"
-    >
-      Hier komt een project detailpagina
+  <section class="relative bg-beige">
+    <div class="mx-auto max-w-screen-xl items-center px-8 py-10">
+      <div v-if="error">{{ error }}</div>
+      <div v-else-if="project">
+        <h1 class="text-center text-3xl text-licorice md:text-4xl">
+          {{ project.title }}
+        </h1>
+        <img class="w-full object-cover max-h-144" :src="project.image.url"/>
+        <p v-for="content in projectContent">
+          <div :key="content._id" v-html="content.html" />
+        </p>
+      </div>
+      <p v-if="!project">Er zijn op dit moment geen actuele projecten.</p>
     </div>
   </section>
 </template>
