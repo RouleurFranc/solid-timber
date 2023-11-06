57d8e478f7726f9c7f4f58aded0c19fa7f547b44
Frank de Boer <frank.de.boer@eo.nl>
2023-11-06 14:56:28 +0100

    added tag manager

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -37,7 +37,6 @@ export default defineNuxtConfig({
     '@nuxt/image',
     'nuxt-simple-sitemap',
     'nuxt-simple-robots',
-    '@zadigetvoltaire/nuxt-gtm',
   ],
   apollo: {
     clients: {
@@ -49,22 +48,6 @@ export default defineNuxtConfig({
   runtimeConfig: {
     public: {
       PREPR_ACCESS_TOKEN: process.env.PREPR_ACCESS_TOKEN,
-      gtm: {
-        id: 'GTM-KQP6FFLK',
-        queryParams: {
-          gtm_auth: 'LCNvKI6KvGVq5YINHcysXQ',
-          gtm_preview: 'env-1',
-          gtm_cookies_win: 'x',
-        },
-        defer: false,
-        compatibility: false,
-        enabled: true,
-        debug: true,
-        loadScript: false,
-        enableRouterSync: true,
-        trackOnNextTick: false,
-        devtools: true,
-      },
     },
   },
   css: ['~/assets/css/main.css'],
