50e352e07b17b7e90d7979efddb358e7f93ca663
Frank de Boer <frank.de.boer@eo.nl>
2023-11-09 21:23:00 +0100

    new layout

---
+++ layouts/project.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="flex flex-col font-body">
+    <Header
+      background-color="green-accent"
+      text-color="green-dark"
+    />
+    <slot />
+    <Footer />
+  </div>
+</template>
